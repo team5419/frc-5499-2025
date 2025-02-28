9ddc5405466f965cd2e8756c97eab9c6e2ab1c98
deltea <thcheetah777@gmail.com>
2025-02-27 16:28:57 -0800

    add disloger subsystem

---
+++ src/main/java/frc/robot/subsystems/Disloger.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+
+public class Disloger extends SubsystemBase {
+  private final SparkMax motor = new SparkMax(RobotMap.DISLOGER, MotorType.kBrushless);
+
+  // public Command getDislogeCommand(int direction)
+}
