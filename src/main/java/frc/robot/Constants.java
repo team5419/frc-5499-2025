0baf22205af09d18e047cd2d01388a83a9425204
deltea <thcheetah777@gmail.com>
2025-03-27 16:50:02 -0700

    stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -35,7 +35,6 @@ public final class Constants {
     REPLAY
   }
 
-  public static final double[] elevatorPositions = /*{0, 95, 134}*/{0, 3.5, 14.4};
+  public static final double[] elevatorPositions = /*{0, 95, 134}*/{0, 3.5, 14.6};
   public static final double elevatorConversion = /*0.0368421053*/1;
 }
-
