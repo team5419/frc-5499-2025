a040652c4be6258f625a3b13ffac25a1bf135981
elliebuehler <50064010+elliebuehler@users.noreply.github.com>
2025-04-15 10:26:28 -0700

    figured out the rev/sparkmax import issue and fixed it

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,5 +1,20 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotMap;
+import org.littletonrobotics.junction.Logger;
+
 public class IntakeSubsystem {
-    
+  private final CANSparkMax intake = new CANSparkMax(RobotMap.INTAKE, MotorType.kBrushless);
+
+  public Command getDislogeCommand(int direction) {
+    return this.runOnce(() -> intake.set(direction));
+  }
+
+  @Override
+  public void periodic() {
+    Logger.recordOutput("Disloger Subsystem/Disloger Motor Output", intake.get());
+  }
 }
