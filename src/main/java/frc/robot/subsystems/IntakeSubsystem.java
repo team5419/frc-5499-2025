52128b2811758cbb34dbfe2ffc0601d035bb86eb
elliebuehler <50064010+elliebuehler@users.noreply.github.com>
2025-04-15 10:48:49 -0700

    complete intake subsystem no errors

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,18 +4,20 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
+import org.littletonrobotics.junction.Logger;
 
-public class IntakeSubsystem {
+public class IntakeSubsystem extends SubsystemBase{
   private final SparkMax intake = new SparkMax(RobotMap.INTAKE, MotorType.kBrushless);
 
   public Command setIntakeCommand(double direction) {
     return Commands.runOnce(() -> intake.set(direction));
   }
-  /*
-    @Override
-    public void periodic() {
-      Logger.recordOutput("Disloger Subsystem/Disloger Motor Output", intake.get());
-    }
-  */
+
+  @Override
+  public void periodic() {
+    Logger.recordOutput("Intake Motor Output", intake.get());
+  }
+  
 }
