3c17e3e6a0aa9f16efd76f37e93f20721c78e358
Dylan-Collier <dylancollier@students.berkeley.net>
2025-04-21 16:01:45 -0700

    imported revrobotics

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+public class RobotMap {
+  // Drivetrain
+  public static final int FRONT_LEFT_ENCODER = 10;
+  public static final int FRONT_LEFT_STEER = 11;
+  public static final int FRONT_LEFT_DRIVE = 12;
+
+  public static final int FRONT_RIGHT_ENCODER = 20;
+  public static final int FRONT_RIGHT_STEER = 21;
+  public static final int FRONT_RIGHT_DRIVE = 22;
+
+  public static final int BACK_LEFT_ENCODER = 30;
+  public static final int BACK_LEFT_STEER = 31;
+  public static final int BACK_LEFT_DRIVE = 32;
+
+  public static final int BACK_RIGHT_ENCODER = 40;
+  public static final int BACK_RIGHT_STEER = 41;
+  public static final int BACK_RIGHT_DRIVE = 42;
+
+  public static final int PIGEON = 50;
+
+  // SparkMAX(intake)
+  public static final int INTAKE = 99;
+}
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,4 +1,8 @@
 package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
