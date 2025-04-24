b5c0826cbf669a9d36808ca48fcdca391f454115
annaraewilliams <annawilliams@students.berkeley.net>
2025-04-24 15:53:55 -0700

    redoing the robot map and subsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,14 +3,11 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
-public class IntakeSystem extends SubsystemBase{
+public class IntakeSubsystem extends SubsystemBase{
     private final SparkMax intake = new SparkMax(RobotMap.INTAKE, MotorType.kBrushless);
 }
-
-public class IntakeSubsystem {
-    
-}
-
 
