32cef9efa83aab081e56e99307370b3e057539e2
Severin_Whittle <severin.whittle@gmail.com>
2025-05-06 16:20:57 -0700

    Update Climb.java

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -9,12 +9,26 @@ import frc.robot.RobotMap;
 
 //adds what SparkMax and MotorType is
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.servohub.ServoHub.ResetMode;
+import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+
 
 //Creates a class that links to the robot map, so that when you iniciate the command in robotmap, the new motor spins (double(Which is a number variable like boolen))
 //adds a new motor to the system that is from the same id as in CLIMBER in the robot map, and is a brushless type
 public class Climb extends SubsystemBase {
     private final SparkMax climb = new SparkMax(RobotMap.CLIMBER, MotorType.kBrushless);
-    public Command setClimbCommand(double direction)
-    return Commands.runOnce(() -> climb.set(direction));
+    public Command setClimbCommand(double direction) {
+        return Commands.runOnce(() -> climb.set(direction));
+        }
 }
+
+    void setCoastMode() {
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.idleMode(IdleMode.kCoast);
+    
+    // Don't persist parameters since it takes time and this change is temporary
+            spark.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
