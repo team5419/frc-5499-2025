447e08c9ca00d7b3c742ae0a279867330b156e2f
florakoven <florakoven@students.berkeley.net>
2025-05-13 16:32:10 -0700

    5/13/25

--- src/main/java/frc/robot/subsystems/InstakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/InstakeSubsystem.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems;
 
-public class InstakeSubsystem {
-    
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+
+public class InstakeSubsystem extends SubsystemBase{
+    private final SparkMax intake = new SparkMax(RobotMap.INTAKE, MotorType.kBrushless);
 }
